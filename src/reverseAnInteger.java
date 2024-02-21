1b4ad580eb4f9775660ec877e2c9251ac76553dc
mohamedali3521 <mohamedali321k@gmail.com>
2024-02-21 11:53:37 +0530

    reverse an integer

--- src/reverseAnInteger.java
+++ src/reverseAnInteger.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class reverseAnInteger {
+public class reverseAnInteger {
+    public static int reverse(int x) {
+        StringBuilder s = new StringBuilder();
+        s.append(Math.abs(x));
+        s.reverse();
+        if (s.length() >= 10) {
+            int c1 = Integer.parseInt(s.substring(0, 5));
+            int c2 = Integer.parseInt(s.substring(5, 10));
+            if (c1 > 21474 || c2 > 83647) {
+                return 0;
+            }
+        }
+
+        int num = Integer.parseInt(s.toString());
+
+        return (x < 0) ? -num : num;
+    }
+
+    public static void main(String[] args) {
+        int x = -260;
+        System.out.println(reverse(x));
+
+    }
 }
